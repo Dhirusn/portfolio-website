115bd15a4cd5883c666cfea241484fde537a6d57
Dhirendra Singh Negti <Dhirendra.academy@outlook.com>
2024-05-30 15:53:16 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { BlogComponent } from './features/blog/blog.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ServiceComponent } from './pages/service/service.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { SingleBlogComponent } from './features/single-blog/single-blog.component';
+import { PortfolioDetailsComponent } from './pages/portfolio-details/portfolio-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'portfolio-details', component: PortfolioDetailsComponent },
+  { path: 'service', component: ServiceComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'single-blog', component: SingleBlogComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { PortfolioDetailsComponent } from './pages/portfolio-details/portfolio-details.component';
+import { ServiceComponent } from './pages/service/service.component';
+import { SingleBlogComponent } from './features/single-blog/single-blog.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogComponent } from './features/blog/blog.component';
+import { TestimonialComponent } from './pages/testimonial/testimonial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    PortfolioComponent,
+    PortfolioDetailsComponent,
+    ServiceComponent,
+    SingleBlogComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    BlogComponent,
+    TestimonialComponent,
   ],
   imports: [
     BrowserModule,
